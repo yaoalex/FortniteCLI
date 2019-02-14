cd69e05d9c37d5e4657b92d56ea16f5ad1c29e23
yaoalex <alexyao1997@gmail.com>
2019-02-14 04:27:30 -0500

    added unit tests for getplayerstats

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,6 +1,58 @@
 package cmd
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type PlayerClient interface {
+	MakeRequest(url string) (map[string]interface{}, error)
+}
+
+type PlayerStats struct {
+	Client PlayerClient
+}
+
+type BasePlayerClient struct{}
+
+func (ps *PlayerStats) GetPlayerStats(player string) (map[string]interface{}, error) {
+	uidURL := fmt.Sprintf(`https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=%s`, player)
+	uidData, err := ps.Client.MakeRequest(uidURL)
+	if err != nil {
+		return nil, err
+	}
+	uid := uidData["uid"]
+	if uid == nil {
+		return nil, nil
+	}
+
+	statsURL := fmt.Sprintf(`https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats?user_id=%s&platform=pc`, uidData["uid"])
+	statsData, err := ps.Client.MakeRequest(statsURL)
+	if err != nil {
+		return nil, err
+	}
+	return statsData, nil
+}
+
+func (bpc *BasePlayerClient) MakeRequest(url string) (map[string]interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
 
 func printStats(stats map[string]interface{}) {
 	fmt.Printf("Solo: %.2f|%v    Duo: %.2f|%v    Squad: %.2f|%v \n", stats["kd_solo"], stats["kills_solo"],
